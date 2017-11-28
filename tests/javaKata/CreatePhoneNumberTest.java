7bd38d89adefb4f069163184e0b2c29368e3740a
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-11-28 19:42:22 +0100

    Create Phone Number Kata (6kyu)

---
+++ tests/javaKata/CreatePhoneNumberTest.java
@@ -0,0 +1,11 @@
+package javaKata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CreatePhoneNumberTest {
+    @Test
+    public void createPhoneNumber() throws Exception {
+        assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+}
