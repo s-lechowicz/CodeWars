3c4325b7b1c6b32b20afe0e69890bbe1f39305d3
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-11-28 19:47:14 +0100

    Factorial Kata (7kyu)

---
+++ src/javaKata/Factorial.java
@@ -0,0 +1,19 @@
+package javaKata;
+
+public class Factorial {
+
+    public int factorial(int n) {
+
+        int output =1;
+
+        if (n < 0 || n > 12){
+            throw new  IllegalArgumentException();
+        }else if(n==0){
+        }else{
+            for (int i=2; i <= n; i++){
+                output *= i;
+            }
+        }
+        return output;
+    }
+}
---
+++ tests/javaKata/FactorialTest.java
@@ -0,0 +1,36 @@
+package javaKata;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class FactorialTest {
+
+    private Factorial fact;
+
+    @Before
+    public void initFactorial() {
+        fact = new Factorial();
+    }
+
+    @After
+    public void afterFactorial() {
+        fact = null;
+    }
+
+    @Test
+    public void test_factorial0() {
+        assertEquals(1, fact.factorial(0));
+    }
+
+    @Test
+    public void test_factorial3() {
+        assertEquals(6, fact.factorial(3));
+    }
+
+    @Test
+    public void test_factorial5() {
+        assertEquals(120, fact.factorial(5));
+    }
+}
