f4195e1012bbbe48e77779effc0196fe91a0a314
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-11-28 19:39:29 +0100

    Multiply Kata (8kyu)

---
+++ src/javaKata/Multiply.java
@@ -0,0 +1,7 @@
+package javaKata;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
---
+++ tests/javaKata/MultiplyTest.java
@@ -0,0 +1,12 @@
+package javaKata;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MultiplyTest {
+    @Test
+    public void multiply() throws Exception {
+        assertEquals("5.0", Multiply.multiply(2.5,2.0).toString());
+    }
+
+}
