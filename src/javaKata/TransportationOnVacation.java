52c0ac909904729691be706b13b39389d139b311
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-12-04 14:08:50 +0100

    Transportation on vacation Kata (8kyu)

---
+++ src/javaKata/TransportationOnVacation.java
@@ -0,0 +1,13 @@
+package javaKata;
+
+public class TransportationOnVacation {
+    public static int rentalCarCost(int d) {
+        if (d >= 7) {
+            return (d * 40) - 50;
+        } else if (d >= 3) {
+            return (d * 40) - 20;
+        } else {
+            return d * 40;
+        }
+    }
+}
